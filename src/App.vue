0037277f0184b511bb70ac704079f5ae7a87b46a
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2023-12-10 17:48:06 +0200

    interactive header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref, watchEffect } from 'vue';
+import { useRouter, useRoute, RouterLink, RouterView } from 'vue-router';
+import { useGalleriaStore } from '@/stores/galleria';
+
+const route = useRoute();
+const router = useRouter();
+const currentSlideSlug = ref(route.params.slideSlug);
+const stopSlideShow = ref(false);
+
+const slideShowButtonLabel = ref(currentSlideSlug.value ? 'STOP SLIDESHOW' : 'START SLIDESHOW');
+const galleria = useGalleriaStore();
+
+const initHeaderButton = (slug) => {
+  slideShowButtonLabel.value = (slug ? 'STOP SLIDESHOW' : 'START SLIDESHOW');
+  stopSlideShow.value = !!slug;
+}
+
+const goHeader = () => {
+  if (stopSlideShow.value) {
+    router.push({ path: '/' })
+  } else {
+    const cardInfo = galleria.getSlideByIndex(0);
+    router.push({ path: '/' + cardInfo.slug })
+  }
+}
+
+watchEffect(() => initHeaderButton(route.params.slideSlug));
 </script>
 
 <template>
@@ -10,7 +36,9 @@ import { RouterLink, RouterView } from 'vue-router'
       </RouterLink>
 
       <nav>
-        <RouterLink to="/" class="text-link-1">START SLIDESHOW</RouterLink>
+        <a href class="text-link-1" @click.prevent="goHeader">
+          {{ slideShowButtonLabel }}
+        </a>
       </nav>
     </div>
   </header>
